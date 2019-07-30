74ae4cc3df9957bf92896c61f114dcb11dfa0b3b
Michael Araya <michaelarayamurcia@gmail.com>
2019-07-30 14:00:09 -0600

    Programando space invaders

--- Invaders/main.js
+++ Invaders/main.js
@@ -1,83 +1,161 @@
-class Ship{
+class Juego{
     constructor(){
-        this.x1 = 200;   //---- Punto izquierdo
-        this.y1 = 390;
-        
-        this.x2 = 220;   //---- Punto medio 
-        this.y2 = 340;
-        
-        this.x3 = 240;   //---- Punto derecho
-        this.y3 = 390;
-        
-        this.dx = 0;     //---- Aceleración
-    }
-    show(){
-        noStroke();
-        fill(0,0,255);
-        triangle(this.x1,this.y1,this.x2,this.y2,this.x3,this.y3);
-    }
-    update(){
-        this.x1 += this.dx;
-        this.x2 += this.dx;
-        this.x3 += this.dx;
+        this.largo = 300;
+        this.ancho = 300;
+        this.velocidad = 3;
+        this.columnaenemigos = 5;
+        this.filadeenemigos = 3;
+        this.direccion = false;
+        this.enemigos = [];
     }
 }
 
-class Enemy{
+class Nave{
     constructor(){
-        this.x = 0;
-        this.y = 0;
-        this.largo = 40;
-        this.ancho = 10;
+        this.x = juego.largo / 2;
+        this.y = juego.ancho - 30;
+        this.dx = 0;
+        this.largo = 20;
+        this.ancho = 30;
+        this.balas = [];
     }
     show(){
-        noStroke();
-        fill(0,255,255);
+        this.x += this.dx;
+        if(this.x + this.largo > juego.largo || this.x < 0){
+            this.x += -this.dx;
+        }
         rect(this.x,this.y,this.largo,this.ancho);
     }
 }
 
-class Bullet{
-    constructor(){
-        this.x = 100//ship.x2;
-        this.y = 100//ship.y2;
-        this.r = 5;
+class Enemigo{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.largo = 20;
+        this.ancho = 20;
     }
     show(){
-        noStroke();
-        fill(255,255,0);
-        ellipse(this.x,this.y,this.r,this.r);
+        fill(0,255,0);
+        rect(this.x,this.y,this.largo,this.ancho);
+    }
+    update(){
+        if (juego.direccion === true){
+            this.x += juego.velocidad;
+        }
+        else{
+            this.x -= juego.velocidad;
+        }
     }
 }
 
-speed = 3;
+class Bala{
+    constructor(){
+        this.x = nave.x + nave.largo / 2;
+        this.y = nave.y - 20;
+        this.largo = 5;
+        this.ancho = 20;
+        this.hit = false;
+    }
+    show(){
+        fill(255,0,0);
+        rect(this.x,this.y,this.largo,this.ancho);
+    }
+    update(){
+        this.y -= juego.velocidad; 
+    }
+}
 
-function keyReleased(){ship.dx = 0;}
+function crearenemigos(){
+    for(let j = 0; j < juego.filadeenemigos;j++){
+        for(let i = 0; i < juego.columnaenemigos;i++){
+            enemigo = new Enemigo( (i + 1) * 40, j * 30 );
+            juego.enemigos.push(enemigo);
+        }
+    }
+}
+
+function disparar(){
+    if(nave.balas.length > 0){
+        for(let i in nave.balas){
+            if(nave.balas[i].hit === true){
+                nave.balas.splice(i,1);
+            }
+            else {
+                nave.balas[i].update();
+            }
+        }
+    }
+}
+
+function destruir(){
+    for (let j = nave.balas.length - 1; j >= 0; j--) {
+        for(let i in juego.enemigos){
+            if(nave.balas[j].y < juego.enemigos[i].y + juego.enemigos[i].largo && 
+                nave.balas[j].x < juego.enemigos[i].x + juego.enemigos[i].ancho &&
+                nave.balas[j].x > juego.enemigos[i].x){
+                nave.balas[j].hit = true;
+                juego.enemigos.splice(i,1);
+            }
+        }
+        if((nave.balas[j].y + nave.balas[j].largo) < 0){
+            console.log("Bala eliminada");
+            nave.balas[j].hit = true;
+        }
+    }
+}
+
+function actualizar(){
+    background(0);
+    nave.show();
+    destruir();
+    disparar();
+    for (let i in juego.enemigos){
+        juego.enemigos[i].show();
+        juego.enemigos[i].update();
+    }
+    for(let j in nave.balas){
+        nave.balas[j].show();
+    }
+    if(juego.enemigos.length > 0){
+        if(juego.enemigos[0].x < 0 || 
+            juego.enemigos[0].x + juego.enemigos[0].largo > juego.largo){
+            juego.direccion = !juego.direccion;
+        }
+    }
+    text("El numero de balas es: " + nave.balas.length,0,200);
+    text("El numero de enemigos es: " + juego.enemigos.length,0,220);
+}
 
 function keyPressed(){
-    switch(keyCode){
-        case(RIGHT_ARROW):
-            ship.dx = speed;
-            break;
-        case(LEFT_ARROW):
-            ship.dx = -speed;
-            break;
-        case(UP_ARROW):
-            bullet.show();
-            break;
+    if(keyCode == RIGHT_ARROW){
+        nave.dx += juego.velocidad; 
+    }
+    else if(keyCode == LEFT_ARROW){
+        nave.dx -= juego.velocidad;
+    } 
+    else if(keyCode == UP_ARROW){
+        bala = new Bala();
+        nave.balas.push(bala);
+        for(let i = 0; i < nave.balas.length;i++){
+            nave.balas[i].update();
+        }
+    }
+}
+
+function keyReleased(){
+    if (key != ' ') {
+        nave.dx = 0;
     }
 }
 
 function setup(){
-    createCanvas(400,400)
-    ship = new Ship();
-    enemy = new Enemy();
-    bullet = new Bullet();
+    juego = new Juego();
+    nave = new Nave();
+    crearenemigos();
+    createCanvas(juego.largo,juego.ancho);
 }
 
 function draw(){
-    background(0);
-    ship.show();
-    ship.update();
-    enemy.show();
+    actualizar();
 }
--- Tetris/main.js
+++ Tetris/main.js
@@ -4,32 +4,47 @@ class Juego{
         this.ancho = 500;
         this.columnas = 10;
         this.filas = 20;
+        this.unidad = 20;
     }
 }
 
-const Z = [[[1,1,0],
-            [0,1,1],
-            [0,0,0]],
-
-           [[0,0,1],
-            [0,1,1],
-            [0,1,0]],
-
-           [[0,0,0],
+const piezaZ = [
             [1,1,0],
-            [0,1,1]],
+            [0,1,1],
+            [0,0,0],
 
-           [[0,1,0],
+            [0,0,1],
+            [0,1,1],
+            [0,1,0],
+
+            [0,0,0],
             [1,1,0],
-            [1,0,0]]];
+            [0,1,1],
+
+            [0,1,0],
+            [1,1,0],
+            [1,0,0]
+                    ];
 
 function actualizar(){
+    //Crear tablero
     for(let i = 0; i < juego.columnas; i++){
         for(let j = 0; j < juego.filas; j++ ){
             fill(255,255,255);
-            rect(i * 20,j * 20,20,20);
+            rect(i * 20,j * 20,juego.unidad,juego.unidad);
         }
     }
+}
+
+function mostrar(){
+        //Mostrar piezas
+        for(let j = 0; j < 2;j++){
+            if(piezaZ[0][j] == 1){
+                //console.log("Funciona" + piezaZ[i][j]);
+                fill(0,255,255);
+                rect(j * juego.unidad,j * juego.unidad,juego.unidad,juego.unidad);
+            }
+        }
 }
 
 function setup(){
@@ -40,4 +55,5 @@ function setup(){
 function draw(){
     background(0);
     actualizar();
+    mostrar();
 }
