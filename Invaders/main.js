350f4d9912a450524d24b7577de3aa9bd26d67b7
Michael Araya <michaelarayamurcia@gmail.com>
2019-08-09 09:14:28 -0600

    Colocando el ordenamiento

--- Invaders/main.js
+++ Invaders/main.js
@@ -1,24 +1,26 @@
 class Juego{
     constructor(){
-        this.largo = 300;
+        this.largo = 350;
         this.ancho = 300;
         this.velocidad = 3;
         this.columnaenemigos = 5;
         this.filadeenemigos = 3;
-        this.balas = 3;
+        this.balas = 2;
         this.direccion = false;
         this.disparo = false;
         this.enemigos = [];
+        this.unidad = 30;
+        this.sonido = new Audio("http://k007.kiwi6.com/hotlink/7di9miccg9/sfx_laser1.ogg");
     }
 }
 
 class Nave{
     constructor(){
         this.x = juego.largo / 2;
-        this.y = juego.ancho - 30;
+        this.y = juego.ancho - juego.unidad;
         this.dx = 0;
-        this.largo = 20;
-        this.ancho = 30;
+        this.largo = juego.unidad;
+        this.ancho = juego.unidad;
         this.balas = [];
     }
     show(){
@@ -34,8 +36,8 @@ class Enemigo{
     constructor(x,y){
         this.x = x;
         this.y = y;
-        this.largo = 20;
-        this.ancho = 20;
+        this.largo = juego.unidad;
+        this.ancho = juego.unidad;
     }
     show(){
         image(enemy,this.x,this.y,this.largo,this.ancho);
@@ -55,7 +57,7 @@ class Bala{
         this.x = nave.x + nave.largo / 2;
         this.y = nave.y - 20;
         this.largo = 5;
-        this.ancho = 20;
+        this.ancho = 10;
         this.hit = false;
     }
     show(){
@@ -65,11 +67,33 @@ class Bala{
         this.y -= juego.velocidad; 
     }
 }
+function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+function ordenar(array) {
+  for(var i = 0; i < array.length; i++) {
+    for(var j = 1; j < array.length; j++) {
+      if(array[j - 1].x > array[j].x && array[j - 1].y < array[j].y) {
+        swap(array, j - 1, j);
+      }
+    }
+  }
+  return array;
+}
+    
+function rebotar(){
+    if(juego.enemigos[0].x < 0 || juego.enemigos[juego.enemigos.length - 1].x + juego.enemigos[juego.enemigos.length - 1].largo > juego.largo){
+        juego.direccion = !juego.direccion;
+    }
+}
 
 function crearenemigos(){
     for(let j = 0; j < juego.filadeenemigos;j++){
         for(let i = 0; i < juego.columnaenemigos;i++){
-            enemigo = new Enemigo( (i + 1) * 40, j * 30 );
+            enemigo = new Enemigo( (i + 1) * 40, j * 34 );
             juego.enemigos.push(enemigo);
         }
     }
@@ -79,6 +103,7 @@ function disparar(){
     if(nave.balas.length < juego.balas){
         bala = new Bala();
         nave.balas.push(bala);
+        juego.sonido.play();
     }   
 }
 
@@ -90,6 +115,7 @@ function destruir(){
                 nave.balas[j].x > juego.enemigos[i].x){
                 nave.balas[j].hit = true;
                 juego.enemigos.splice(i,1);
+                ordenar(juego.enemigos);
             }
         }
         if((nave.balas[j].y + nave.balas[j].largo) < 0){
@@ -107,6 +133,9 @@ function actualizar(){
     image(backgrounds,0,0,juego.largo,juego.ancho);
     nave.show();
     destruir();
+    if(juego.enemigos.length > 0){
+    rebotar();
+    }
     for (let i in juego.enemigos){
         juego.enemigos[i].show();
         juego.enemigos[i].update();
@@ -115,14 +144,6 @@ function actualizar(){
         nave.balas[j].show();
         nave.balas[j].update();
     }
-    if(juego.enemigos.length > 0){
-        if(juego.enemigos[0].x < 0 || 
-            juego.enemigos[0].x + juego.enemigos[0].largo > juego.largo){
-            juego.direccion = !juego.direccion;
-        }
-    }
-    text("El numero de balas es: " + nave.balas.length,0,200);
-    text("El numero de enemigos es: " + juego.enemigos.length,0,220);
 }
 
 function touchStarted(){
